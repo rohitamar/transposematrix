3148f5fd7149df785104be31f72266a8bc5207e2
Rohit <rohitamar@gmail.com>
2025-02-14 00:27:02 -0500

    ?, changed?!?! idek

--- main.cpp
+++ main.cpp
@@ -2,43 +2,43 @@
 #include <memory>
 #include <algorithm>
 #include <numeric>
+#include <immintrin.h>
 
-typedef long long int ll;
+const int R = 1LL << 8;
+const int C = 1LL << 8;
 
-const ll R = 1LL << 20;
-const ll C = 1LL << 20;
-
-ll mat[R][C];
-ll transposed[C][R];
+alignas(32) int mat[R][C];
+alignas(32) int transposed[C][R];
 
 void fill() {
-    for(ll i = 0; i < C; i++) {
+    for(int i = 0; i < C; i++) {
         std::fill_n(transposed[i], R, 0LL);
     }
-    for(ll i = 0; i < R; i++) {
+    for(int i = 0; i < R; i++) {
         std::fill_n(mat[i], C, 0LL);
         std::iota(mat[i] + i, mat[i] + C, 1LL);
     }
 }
 
 void bf() {
-    for(ll i = 0; i < R; i++) {
-        for(ll j = 0; j < C; j++) {
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
             transposed[j][i] = mat[i][j];
         }
     }
 }
 
-void sse() {
-
+void avx() {
+    for(int i = 0; i < R / 8; i++) {
+        for(int j = 0; j < R / 8; j++) {
+            __m256i row0 = _mm256_load_si256(reinterpret_cast<__m256i*>(&mat[i][j]));
+            __m256i row1 = _mm256_load_si256(reinterpret_cast<__m256i*>(&mat[i + 1][j]));
+            __m256i row2 = _mm256_load_si256(reinterpret_cast<__m256i*>(&mat[i + 2][j]));
+            __m256i row3 = _mm256_load_si256(reinterpret_cast<__m256i*>(&mat[i + 3][j]));
+        }
+    }
 }
 
 int main() {
-    fill();
-    bf();
-    
-    fill();
-    sse();
-    
-    fill();
+
 }
