da2b53bfd1c17a4e95970fb93de67e4738104ba7
Rohit <rohitamar@gmail.com>
2025-02-14 22:42:33 -0500

    pc being weird

--- main.cpp
+++ main.cpp
@@ -9,21 +9,25 @@
 
 using namespace std::chrono;
 
-const int R = 1LL << 13;
-const int C = 1LL << 13;
+const int R = 1LL << 5;
+const int C = 1LL << 5;
 
 int main() {
     Matrix<int> mat(R, C);
+    // mat.print();
     auto start = high_resolution_clock::now();
-    mat.bf();
+    Matrix<int> bf_trans = mat.bf();
     auto end = high_resolution_clock::now();
-    double normal_duration = duration_cast<microseconds>(end - start).count();
+    // bf_trans.print();
+    double normal_duration = duration_cast<seconds>(end - start).count();
     std::cout << "Normal Time taken: " << normal_duration << "\n";
 
     start = high_resolution_clock::now(); 
-    mat.sse();
+    // Matrix<int> trans = mat.sse44();
+    // trans.print();
     end = high_resolution_clock::now();
     double sse_duration = duration_cast<microseconds>(end - start).count();
     std::cout << "SSE Time taken: " << sse_duration << "\n";
-    std::cout << "Ratio: " << normal_duration / sse_duration << "\n";
+
+    // std::cout << "Ratio: " << normal_duration / sse_duration << "\n";
 }
--- matrix.hpp
+++ matrix.hpp
@@ -31,7 +31,7 @@ class Matrix {
     inline void print() const {
         for(size_t i = 0; i < rows_; i++) {
             for(size_t j = 0; j < cols_; j++) {
-                std::cout << (this)(i, j) << " ";
+                std::cout << this->operator()(i, j) << " ";
             }
             std::cout << "\n";
         }
@@ -91,14 +91,14 @@ Matrix<T> Matrix<T>::avx88() {
 
     for(size_t i = 0; i < rows_ / 8; i++) {
         for(size_t j = 0; j < cols_ / 8; j++) {
-            __m256i row0 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i    , j))));
-            __m256i row1 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i + 1, j))));
-            __m256i row2 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i + 2, j))));
-            __m256i row3 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i + 3, j))));
-            __m256i row4 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i + 4, j))));
-            __m256i row5 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i + 5, j))));
-            __m256i row6 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i + 6, j))));
-            __m256i row7 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator(i + 7, j))));
+            __m256i row0 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i    , j))));
+            __m256i row1 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i + 1, j))));
+            __m256i row2 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i + 2, j))));
+            __m256i row3 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i + 3, j))));
+            __m256i row4 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i + 4, j))));
+            __m256i row5 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i + 5, j))));
+            __m256i row6 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i + 6, j))));
+            __m256i row7 = _mm256_loadu_si256(reinterpret_cast<__m256i*>(&(this->operator()(i + 7, j))));
 
             __m256i first0 = _mm256_unpacklo_epi32(row0, row1); // a0 b0 a1 b1 a2 b2 a3 b3
             __m256i first1 = _mm256_unpackhi_epi32(row0, row1); // a4 b4 a5 b5 a6 b6 a7 b7 
@@ -113,7 +113,28 @@ Matrix<T> Matrix<T>::avx88() {
             __m256i second1 = _mm256_unpackhi_epi64(first0, first2); // a2 b2 c2 d2 a3 b3 c3 d3
             __m256i second2 = _mm256_unpacklo_epi64(first1, first3); // a4 b4 c4 d4 a5 b5 c5 d5
             __m256i second3 = _mm256_unpackhi_epi64(first1, first3); // a6 b6 c6 d6 a7 b7 c7 d7
+            __m256i second4 = _mm256_unpacklo_epi64(first4, first6); // e0 f0 g0 h0 e1 f1 g1 h1
+            __m256i second5 = _mm256_unpackhi_epi64(first4, first6); // e2 f2 g2 h2 e3 f3 g3 h3
+            __m256i second6 = _mm256_unpacklo_epi64(first5, first7); // e4 f4 g4 h4 e5 f5 g5 h5
+            __m256i second7 = _mm256_unpackhi_epi64(first5, first7); // e6 f6 h6 g6 e7 f7 g7 h7
 
+            __m256i col0 = _mm256_permute2x128_si256(second0, second4, 0x20);
+            __m256i col1 = _mm256_permute2x128_si256(second1, second5, 0x20);
+            __m256i col2 = _mm256_permute2x128_si256(second2, second6, 0x20);
+            __m256i col3 = _mm256_permute2x128_si256(second3, second7, 0x20);
+            __m256i col4 = _mm256_permute2x128_si256(second0, second4, 0x31);
+            __m256i col5 = _mm256_permute2x128_si256(second1, second5, 0x31);
+            __m256i col6 = _mm256_permute2x128_si256(second2, second6, 0x31);
+            __m256i col7 = _mm256_permute2x128_si256(second3, second7, 0x31);
+
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j    , i)), col0);
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j + 1, i)), col1);
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j + 2, i)), col2);
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j + 3, i)), col3);
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j + 4, i)), col4);
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j + 5, i)), col5);
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j + 6, i)), col6);
+            _mm256_storeu_si256(reinterpret_cast<__m256i*>(&transposed(j + 7, i)), col7);
         }
     }
 }
